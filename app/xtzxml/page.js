e856767eef55b3816d2ddd792bd2a2aba898ff00
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2024-09-21 17:34:23 -0500

    Update page.js
    nuevo ancho propiedad

--- app/xtzxml/page.js
+++ app/xtzxml/page.js
@@ -441,8 +441,8 @@ export const DasboarthInico = () => {
         </ul>
 
         {/* Contenido de la pagina web .*/}
-        <div className="contenidoApp  ">
-          <div className="portafolioPerfil">
+        <div className="contenidoApp  w-[100%] ">
+          <div className="portafolioPerfil   w-[100%]">
             <h2>PORTAFOLIO</h2>
             <p>
               Cordial saludo soy <span> FRANKLIM DE JESUS MUÑOZ VALVERDE</span>{" "}
